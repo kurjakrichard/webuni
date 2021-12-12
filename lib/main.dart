22bb6835e0950fe1ce84da8d8cd8da12ae4659d2
Kurják Richárd <kurjak.richard@gmail.com>
2021-12-12 22:30:30 +0100

    daily update

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,6 @@
 //A material design kell a fejlesztéshez.
 import 'package:flutter/material.dart';
+import 'package:webuni/screens/stateful_test.dart';
 
 // Célszerű külön osztályban csinálni a widgeteket, még a főablakét is.
 void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int count = 0;
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Column(
-        children: [
-          ElevatedButton(
-            child: const Text('Press me'),
-            onPressed: () {
-              setState(() {
-                count++;
-              });
-            },
-          ),
-          Text('Counter value: $count'),
-        ],
-      ),
-    );
-  }
-}
